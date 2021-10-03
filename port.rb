0f660d17d4cf02ce442459504d93a3c423dcaf81
KlausXie <xie.jinye@163.com>
2021-10-03 16:40:05 +0800

    update

--- port.rb
+++ port.rb
@@ -3,8 +3,10 @@ require 'formula'
 class Port < Foumula 
   homepage ''
   url 'https://raw.githubusercontent.com/Mkluas/homebrew-xie/master/port.sh'
+  sha256 ''
 
   def install
+    system 'make'
   end
 
   test do 
