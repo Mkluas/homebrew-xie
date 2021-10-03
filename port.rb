8ced8b3289b305137180dd6eca39d7b3614b0b38
KlausXie <xie.jinye@163.com>
2021-10-03 16:43:43 +0800

    update

--- port.rb
+++ port.rb
@@ -1,15 +1,27 @@
 require 'formula'
 
-class Port < Foumula 
-  homepage ''
-  url 'https://raw.githubusercontent.com/Mkluas/homebrew-xie/master/port.sh'
-  sha256 ''
+class Armitage < Formula
+  homepage 'http://www.fastandeasyhacking.com/'
+  url 'http://www.fastandeasyhacking.com/download/armitage150813.tgz'
+  sha256 '288065a8b779b6a4f4acaa0332f2ebbfd022529f600e4bf70cd494d84860f6ac'
+  version '150813'
+  revision 2
+
+  depends_on :java => "1.7+"
 
   def install
-    system 'make'
+    libexec.install Dir['*']
+    (bin/"armitage").write <<~EOS
+      #!/bin/sh
+      cd #{libexec} && ./armitage "$@"
+    EOS
+    (bin/"teamserver").write <<~EOS
+      #!/bin/sh
+      cd #{libexec} && ./teamserver "$@"
+    EOS
   end
 
-  test do 
-    port
+  test do
+    #system "#{bin}/blah"
   end
 end
