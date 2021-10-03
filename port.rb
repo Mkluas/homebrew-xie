8eaaaa3429036bf0ceac5f08c4ec98ba68368a40
KlausXie <xie.jinye@163.com>
2021-10-03 17:11:53 +0800

    update

--- port.rb
+++ port.rb
@@ -4,6 +4,7 @@ class Port < Formula
   homepage 'http://www.baidu.com'
   url 'https://raw.githubusercontent.com/Mkluas/homebrew-xie/master/port.tgz'
   sha256 '0bace4d78eef7de4bb465c59afeb5c2b01fbf2b5645d58d66683a049f4a1710c'
+  version 1
 
   def install
     
